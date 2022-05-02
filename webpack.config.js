bcf0b7180d7b0f94ebf0942fc9dcb5c56ebe2f9b
Igor Zubarev <intactowow@gmail.com>
2022-05-02 14:26:22 +0300

    template

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+
+module.exports = {
+  mode: mode,
+  entry: {
+    scripts: './src/index.js',
+  },
+  output: {
+    filename: 'js/build.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name][ext][query]',
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {loader: "css-loader"},
+          {loader: "sass-loader"}
+        ]
+      },
+      {
+        test: /\.(png|svg|webp|jpg|jpeg|gif|json)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|)$/,
+        type: 'asset/resource',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024 // 8kb
+          }
+        },
+        generator: {
+          filename: 'fonts/[name][ext][query]'
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // options: {
+          //     presets: ['@babel/preset-env']
+          // }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: "./src/images/", to: "images"},
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.pug",
+      filename: "home.html"
+    }),
+  ],
+}
